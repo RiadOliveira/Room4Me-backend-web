ef85fab877aaea9806004843a99e4b97da8b35e0
RiadOliveira <riad.oliveira@hotmail.com>
2023-02-07 13:59:56 -0300

    Initial commit configurations

---
+++ src/main/java/com/room4me/Room4meApplication.java
@@ -0,0 +1,12 @@
+package com.room4me;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Room4meApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(Room4meApplication.class, args);
+	}
+
+}
